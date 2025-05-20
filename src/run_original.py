6887191f55a07d16fa14cf382c4e0cbbaec6c334
decade <20120220@student.hcmus.edu.vn>
2025-05-20 16:04:35 +0700

    Correct the train scripts.

--- src/run_original.py
+++ src/run_original.py
@@ -7,11 +7,11 @@ SP500_OPTIMAL_SCALE = 'b657771600cd8b0c40267ece85412e2a21aafc17'
 
 def main() -> None:
     # Parameters
-    market_name: List[str] = ["SP500", "crypto"]
-    stock_num: List[str] = ["474", "117"]
-    valid_index: List[str] = ["1006", "620"]
-    test_index: List[str] = ["1259", "827"]
-    market_values: List[str] = ["8", "10"]
+    market_name: List[str] = ["NASDAQ", "SP500", "crypto"]
+    stock_num: List[str] = ["1026", "474", "117"]
+    valid_index: List[str] = ["756", "1006", "620"]
+    test_index: List[str] = ["1008", "1259", "827"]
+    market_values: List[str] = ["20", "8", "10"]
 
     original_dir = Path(__file__).resolve().parent / "original"
     for i in range(len(market_name)):
@@ -28,11 +28,10 @@ def main() -> None:
             test_index[i],
             market_values[i],
         ]
-
         run(cmd, check=True, cwd=original_dir)
 
         if market_name[i] == "SP500":
-            run(["git", "checkout", "HEAD"], check=True)
+            run(["git", "checkout", "market-context-combined-with-gating"], check=True)
 
 
 if __name__ == "__main__":
--- src/run_original.sh
+++ src/run_original.sh
@@ -10,7 +10,7 @@ for i in "${!market_name[@]}"; do
   if [ "${market_name[$i]}" == "SP500" ]; then
     git checkout b657771600cd8b0c40267ece85412e2a21aafc17
     python3 train.py ${market_name[$i]} ${stock_num[$i]} ${valid_index[$i]} ${test_index[$i]} ${market_values[$i]}
-    git checkout HEAD
+    git checkout market-context-combined-with-gating
   else
     python3 train.py ${market_name[$i]} ${stock_num[$i]} ${valid_index[$i]} ${test_index[$i]} ${market_values[$i]}
   fi
